cf89213e1ef55939f5a956510b4ff94e354e24ec
Alec Thomas <alec@swapoff.org>
2018-05-26 17:38:35 -0400

    Add hook support.

--- _examples/shell/main.go
+++ _examples/shell/main.go
@@ -3,25 +3,29 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/alecthomas/kong"
 )
 
 var CLI struct {
-	Rm struct {
+	Help bool `kong:"help='Display help.'"`
+	Rm   struct {
 		Force     bool `kong:"help='Force removal.'"`
 		Recursive bool `kong:"help='Recursively remove files.'"`
 
-		Paths []string `kong:"help='Paths to remove.',type='path'"`
-	} `kong:"help='Remove files.'"`
+		Paths []string `kong:"arg,help='Paths to remove.',type='path'"`
+	} `kong:"cmd,help='Remove files.'"`
 
 	Ls struct {
 		Paths []string `kong:"help='Paths to list.',type='path'"`
-	} `kong:"help='List paths.'"`
+	} `kong:"cmd,help='List paths.'"`
 }
 
 func main() {
-	cmd := kong.Parse(&CLI)
+	app := kong.Must(&CLI).Hook(&CLI.Help, kong.Help(nil, nil))
+	cmd, err := app.Parse(os.Args[1:])
+	app.FatalIfErrorf(err)
 	s, _ := json.Marshal(&CLI)
 	fmt.Println(cmd)
 	fmt.Println(string(s))
--- help.go
+++ help.go
@@ -1,7 +1,6 @@
 package kong
 
 import (
-	"io"
 	"text/template"
 )
 
@@ -18,20 +17,20 @@ usage: {{.Name}}
 
 var defaultHelpTemplate = template.Must(template.New("help").Parse(defaultHelp))
 
-// WriteHelp to w.
-//
-// If w is nil, the default stdout writer will be used.
-//
-// If args are provided, help will be written in the context o
-func (k *Kong) WriteHelp(w io.Writer, args ...interface{}) error {
-	if w == nil {
-		w = k.stdout
+// Help returns a Hook that will display help and exit.
+func Help(tmpl *template.Template, tmplctx map[string]interface{}) Hook {
+	return func(app *Kong, ctx *Context, trace *Trace) error {
+		merged := map[string]interface{}{
+			"Application": app.Model,
+		}
+		for k, v := range tmplctx {
+			merged[k] = v
+		}
+		err := tmpl.Execute(app.Stdout, merged)
+		if err != nil {
+			return err
+		}
+		app.Exit(0)
+		return nil
 	}
-	ctx := map[string]interface{}{
-		"Application": k.Model,
-	}
-	for k, v := range k.helpContext {
-		ctx[k] = v
-	}
-	return k.help.Execute(w, ctx)
 }
