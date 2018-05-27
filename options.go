afbb43164102631e7fa7ce97d6a991bc62e072cc
Alec Thomas <alec@swapoff.org>
2018-05-27 13:37:17 -0400

    Convert Hook to an Option.

--- options.go
+++ options.go
@@ -2,6 +2,7 @@ package kong
 
 import (
 	"io"
+	"reflect"
 	"text/template"
 )
 
@@ -10,6 +11,13 @@ type Option func(k *Kong)
 // ExitFunction overrides the function used to terminate. This is useful for testing or interactive use.
 func ExitFunction(exit func(int)) Option {
 	return func(k *Kong) { k.Exit = exit }
+}
+
+// NoDefaultHelp disables the default help flags.
+func NoDefaultHelp() Option {
+	return func(k *Kong) {
+		k.noDefaultHelp = true
+	}
 }
 
 // Name overrides the application name.
@@ -39,5 +47,16 @@ func Writers(stdout, stderr io.Writer) Option {
 	return func(k *Kong) {
 		k.Stdout = stdout
 		k.Stderr = stderr
+	}
+}
+
+// Hook to execute when a command, flag or positional argument is encountered.
+func Hook(ptr interface{}, hook HookFunction) Option {
+	key := reflect.ValueOf(ptr)
+	if key.Kind() != reflect.Ptr {
+		panic("expected a pointer")
+	}
+	return func(k *Kong) {
+		k.hooks[key] = hook
 	}
 }
