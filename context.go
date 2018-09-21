25399cde9e5dc612632fbd594f9f92ce9b3c4a87
Alec Thomas <alec@swapoff.org>
2018-09-21 13:51:11 +1000

    Fix erroneously "fixed" error message.

--- context.go
+++ context.go
@@ -552,7 +552,7 @@ func checkMissingChildren(node *Node) error {
 	if len(missing) == 1 {
 		return fmt.Errorf("expected %s", missing[0])
 	}
-	return fmt.Errorf("expected %s", strings.Join(missing, "  "))
+	return fmt.Errorf("expected one of %s", strings.Join(missing, ",  "))
 }
 
 // If we're missing any positionals and they're required, return an error.
