d14caa7316e161ffeeb254c3b33a25aadda16c51
Will Roden <willabides@github.com>
2020-02-25 12:44:28 -0600

    restore CompletionInstaller to CompletionOptions

--- completion.go
+++ completion.go
@@ -6,36 +6,79 @@ import (
 
 // CompletionOptions options for shell completion.
 type CompletionOptions struct {
-	// the function to run for shell completions. If nil, no completions will be run. default: nil
+	// Completer is the function to run for shell completions. If nil, no completions will be run. default: nil
 	Completer Completer
+
+	// CompletionInstaller determines the functions to run when running InstallCompletion and UninstallCompletion.
+	// By default, Install and Uninstall functions from github.com/posener/complete/cmd/install are used.
+	CompletionInstaller CompletionInstaller
 }
 
-// InstallCompletion will install completion to your shell
-type InstallCompletion struct {
-	Installer func(*Context) error
+// CompletionInstaller contains functions to install completions from to a shell.
+type CompletionInstaller interface {
+	Install(ctx *Context) error
+	Uninstall(ctx *Context) error
 }
 
-//Run runs install
-func (c *InstallCompletion) Run(ctx *Context) error {
-	if c.Installer != nil {
-		return c.Installer(ctx)
+// NewCompletionInstaller returns a CompletionInstaller with the given install and uninstall funcs
+// leaving either nil will cause it to defer to the default installer/uninstaller
+func NewCompletionInstaller(install, uninstall func(*Context) error) CompletionInstaller {
+	return &completionInstaller{
+		install:   install,
+		uninstall: uninstall,
 	}
-	// use installer from github.com/posener/complete by default
+}
+
+// completionInstaller is an implementation of CompletionInstaller
+type completionInstaller struct {
+	install   func(ctx *Context) error
+	uninstall func(ctx *Context) error
+}
+
+func (c *completionInstaller) Install(ctx *Context) error {
+	if c.install != nil {
+		return c.install(ctx)
+	}
+	return defaultCompletionInstall(ctx)
+}
+
+func defaultCompletionInstall(ctx *Context) error {
 	return install.Install(ctx.Model.Name)
 }
 
-// UninstallCompletion will uninstall completion from your shell (reverses InstallCompletion)
-type UninstallCompletion struct {
-	Uninstaller func(*Context) error
+func (c *completionInstaller) Uninstall(ctx *Context) error {
+	if c.uninstall != nil {
+		return c.uninstall(ctx)
+	}
+	return defaultCompletionUninstall(ctx)
 }
+
+func defaultCompletionUninstall(ctx *Context) error {
+	return install.Uninstall(ctx.Model.Name)
+}
+
+// InstallCompletion will install completion to your shell
+type InstallCompletion struct{}
+
+//Run runs install
+func (c *InstallCompletion) Run(ctx *Context) error {
+	inst := ctx.Kong.completionOptions.CompletionInstaller
+	if inst == nil {
+		inst = NewCompletionInstaller(nil, nil)
+	}
+	return inst.Install(ctx)
+}
+
+// UninstallCompletion will uninstall completion from your shell (reverses InstallCompletion)
+type UninstallCompletion struct{}
 
 //Run runs uninstall
 func (c *UninstallCompletion) Run(ctx *Context) error {
-	if c.Uninstaller != nil {
-		return c.Uninstaller(ctx)
+	inst := ctx.Kong.completionOptions.CompletionInstaller
+	if inst == nil {
+		inst = NewCompletionInstaller(nil, nil)
 	}
-	// use uninstaller from github.com/posener/complete by default
-	return install.Uninstall(ctx.Model.Name)
+	return inst.Uninstall(ctx)
 }
 
 // Completer is a function to run shell completions. Returns true if this was a completion run. Kong will exit 0
